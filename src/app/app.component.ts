2a3550461efa12c50cfa2a0e555cf4b8a2009013
cyanfox-d <Cyanfox_D@hotmail.com>
2025-04-08 18:39:37 -0700

    cambiando diseño y quitando las opciones extras

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,14 +13,15 @@ import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutlin
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
+    
+    { 
+      title: 'home', 
+      url: '/home', 
+      icon: 'home'
+    },
+    { title: 'Lector credencial', url: '/lector', icon: 'scan-circle-outline'},
+
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {
     addIcons({ mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp });
   }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,4 +11,12 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./folder/folder.page').then((m) => m.FolderPage),
   },
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then( m => m.HomePage)
+  },
+  {
+    path: 'lector',
+    loadComponent: () => import('./lector/lector.page').then( m => m.LectorPage)
+  },
 ];
